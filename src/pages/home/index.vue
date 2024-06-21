0e33a63c3e4801b522f23308f2a9590c60733e73
yinsiyu <516159562@qq.com>
2024-06-21 23:30:45 +0800

    fix: 提交

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,42 +1,73 @@
 <template>
   <view class="page-container">
-    <u-swiper :autoplay="true" :circular="true" :indicator-dots="true" :interval="3000" :list="list"></u-swiper>
-    <u-grid :border="false" :col="4" style="background: black;">
-      <u-grid-item>
-        <u-icon name="photo" :size="46"></u-icon>
-        <view class="grid-text">我的资产</view>
-      </u-grid-item>
-      <u-grid-item>
-        <u-icon name="photo" :size="46"></u-icon>
-        <view class="grid-text">资产日志</view>
-      </u-grid-item>
-      <u-grid-item>
-        <u-icon name="photo" :size="46"></u-icon>
-        <view class="grid-text">账户详情</view>
-      </u-grid-item>
-      <u-grid-item>
-        <u-icon name="photo" :size="46"></u-icon>
-        <view class="grid-text">账户绑定</view>
-      </u-grid-item>
-      <u-grid-item>
-        <u-icon name="photo" :size="46"></u-icon>
-        <view class="grid-text">团队列表</view>
-      </u-grid-item>
-      <u-grid-item>
-        <u-icon name="photo" :size="46"></u-icon>
-        <view class="grid-text">注册邀请</view>
-      </u-grid-item>
-      <u-grid-item>
-        <u-icon name="photo" :size="46"></u-icon>
-        <view class="grid-text">新手教程</view>
-      </u-grid-item>
-      <u-grid-item>
-        <div @click="goToPage('custom')">
-          <u-icon name="photo" :size="46"></u-icon>
-          <view class="grid-text">在线客服</view>
-        </div>
-      </u-grid-item>
-    </u-grid>
+    <u-swiper
+      :autoplay="true"
+      :circular="true"
+      :indicator-dots="true"
+      :interval="3000"
+      :list="list"
+      :height="300"
+    ></u-swiper>
+    <div class="grid-container">
+      <u-grid :border="false" :col="4" border>
+        <u-grid-item
+          bg-color="#ff9900"
+          @click="goToPage('person')"
+          icon-style="color: #007aff"
+        >
+          <u-icon name="account-fill" :size="46"></u-icon>
+          <view class="grid-text">{{ $t("my.myInfo") }}</view>
+        </u-grid-item>
+        <u-grid-item bg-color="#ff9900" @click="goToPage('assets')">
+          <u-icon
+            name="red-packet-fill"
+            :size="46"
+            icon-style="color: '#007aff'"
+          ></u-icon>
+          <view class="grid-text">{{ $t("my.assets") }}</view>
+        </u-grid-item>
+        <u-grid-item bg-color="#ff9900">
+          <u-icon
+            name="photo"
+            :size="46"
+            icon-style="color: '#007aff"
+          ></u-icon>
+          <view class="grid-text">{{ $t("my.tutorial") }}</view>
+        </u-grid-item>
+        <u-grid-item
+          bg-color="#ff9900"
+          @click="goToPage('custom')"
+        >
+          <u-icon name="server-fill" :size="46" icon-style="color: #007aff"></u-icon>
+          <view class="grid-text">{{ $t("my.customer") }}</view>
+        </u-grid-item>
+        <u-grid-item
+          bg-color="#ff9900"
+        >
+          <u-icon name="server-fill" :size="46" icon-style="color: #007aff"></u-icon>
+          <view class="grid-text">{{ $t("my.team") }}</view>
+        </u-grid-item>
+        <u-grid-item
+          bg-color="#ff9900"
+        >
+          <u-icon name="server-fill" :size="46" icon-style="color: #007aff"></u-icon>
+          <view class="grid-text">{{ $t("my.fundLog") }}</view>
+        </u-grid-item>
+        <u-grid-item
+          bg-color="#ff9900"
+        >
+          <u-icon name="server-fill" :size="46" icon-style="color: #007aff"></u-icon>
+          <view class="grid-text">{{ $t("my.Account ") }}</view>
+        </u-grid-item>
+        <u-grid-item
+          bg-color="#ff9900"
+        >
+          <u-icon name="server-fill" :size="46" icon-style="color: #007aff"></u-icon>
+          <view class="grid-text">{{ $t("my.acDetail") }}</view>
+        </u-grid-item>
+      </u-grid>
+    </div>
+
     <view class="currency-info">
       <view
         v-for="currency in currencies"
@@ -86,12 +117,12 @@
 </template>
 
 <script setup>
-import {ref} from "vue";
-import {onHide, onShow} from "@dcloudio/uni-app";
+import { ref } from "vue";
+import { onHide, onShow } from "@dcloudio/uni-app";
 import UImage from "../../uni_modules/vk-uview-ui/components/u-image/u-image.vue";
-import {useTitle} from "@/hooks/useTitle";
-import {useI18n} from "vue-i18n";
-import http from '@/api/http'
+import { useTitle } from "@/hooks/useTitle";
+import { useI18n } from "vue-i18n";
+import http from "@/api/http";
 
 const Base_Url = import.meta.env.VITE_APP_BASE_API;
 
@@ -103,15 +134,18 @@ const hb = import.meta.env.VITE_HUOBI_API;
 // @click="window.open('https://www.moyu-b1.xyz/chat/index/newChat/m/65e844e7bb06f','_blank')"
 const list = ref([
   {
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs0F5_TuVM6mzv27Wr-A_eW9rjUarQ0lss1w&usqp=CAU",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs0F5_TuVM6mzv27Wr-A_eW9rjUarQ0lss1w&usqp=CAU",
     title: "身无彩凤双飞翼，心有灵犀一点通",
   },
   {
-    image: "https://lh5.googleusercontent.com/proxy/JhDH-Fa2INdc9fa1BvSGTXViqgeHjH9ND_wctUk77Qqaoa5fLYPCb13wKAl34rleJ7EBYO46QVkMt-_jILC7Q6N0lC4shB1CaYNFicxg0vR9BIWWtX3wKATDgdIyX3xsMYjSTu7JBHP38DQ",
+    image:
+      "https://lh5.googleusercontent.com/proxy/JhDH-Fa2INdc9fa1BvSGTXViqgeHjH9ND_wctUk77Qqaoa5fLYPCb13wKAl34rleJ7EBYO46QVkMt-_jILC7Q6N0lC4shB1CaYNFicxg0vR9BIWWtX3wKATDgdIyX3xsMYjSTu7JBHP38DQ",
     title: "昨夜星辰昨夜风，画楼西畔桂堂东",
   },
   {
-    image: "https://p1.img.cctvpic.com/photoworkspace/2021/05/21/2021052112010285046.jpg",
+    image:
+      "https://p1.img.cctvpic.com/photoworkspace/2021/05/21/2021052112010285046.jpg",
     title: "谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳",
   },
 ]);
@@ -130,35 +164,43 @@ const fetchCryptoData = async () => {
   currencies.value = allCryptos.filter((crypto) => {
     // 优先展示 BTC、ETH、XRP，这里可以根据 symbol 进行过滤
     return (
-        crypto.symbol === "btcusdt" ||
-        crypto.symbol === "ethusdt" ||
-        crypto.symbol === "solusdt"
+      crypto.symbol === "btcusdt" ||
+      crypto.symbol === "ethusdt" ||
+      crypto.symbol === "solusdt"
     );
   }); // Display the first 4 currencies
   cryptoList.value = allCryptos.slice(0, 10);
 };
 
-const interFetch  = () => {
+const interFetch = () => {
   if (timer.value) {
-    clearInterval(timer.value)
+    clearInterval(timer.value);
   }
   timer.value = setInterval(() => {
     fetchCryptoData();
-  }, 10000)
-}
+  }, 10000);
+};
 const clearFetch = () => {
   if (timer.value) {
-    clearInterval(timer)
+    clearInterval(timer);
   }
-}
+};
 
 const goToPage = (type) => {
-  if (type === 'custom') {
+  if (type === 'person') {
     uni.navigateTo({
-      url: '/pages/custom/index'
-    })
+      url: "/pages/myDetail/index",
+    });
+  } else if (type === 'assets') {
+    uni.navigateTo({
+      url: "/pages/my/assets/index",
+    });
+  } else if (type === "custom") {
+    uni.navigateTo({
+      url: "/pages/custom/index",
+    });
   }
-}
+};
 
 onShow(() => {
   fetchCryptoData();
@@ -167,11 +209,8 @@ onShow(() => {
 onHide(() => {
   clearFetch();
 });
-
-
 </script>
 <style lang="scss" scoped>
-
 .swiper-container {
   width: 100%;
   height: 200px;
@@ -179,7 +218,9 @@ onHide(() => {
   border-radius: 10px;
   //box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
-
+.grid-container {
+  padding-top: 40rpx;
+}
 .swiper-item {
   width: 100%;
   height: 100%;
@@ -188,35 +229,36 @@ onHide(() => {
   position: relative;
 }
 
-
 .page-container {
   padding: 20rpx;
   padding-bottom: 120rpx;
 }
 .currency-info {
+  overflow: hidden;
+  box-sizing: border-box;
   margin-top: 20px;
   display: flex;
   justify-content: space-around;
+  width: 100%;
   padding: 10px 0;
   //background-color: #f8f8f8;
   border-radius: 8px;
-}
-
-.currency-item {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.currency-name {
-  font-size: 16px;
-  font-weight: bold;
-}
-
-.currency-amount {
-  margin-top: 5px;
-  font-size: 14px;
-  color: #ff5454;
+  .currency-item {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    transform: scale(0.7);
+    .currency-name {
+      font-size: 16px;
+      font-weight: bold;
+    }
+    .currency-amount {
+      margin-top: 5px;
+      font-size: 14px;
+      color: #ff5454;
+    }
+  }
 }
 
 .crypto-table {
