01a306d1039bd690426eedd722e381cf4e5dd22b
yinsiyu <516159562@qq.com>
2024-06-22 00:11:55 +0800

    fix: 提交

--- src/components/MyTabBar/index.vue
+++ src/components/MyTabBar/index.vue
@@ -1,64 +1,79 @@
 <template>
-  <u-tabbar :height="100" bg-color="#000" inactive-color="#fff" active-color="#2b85e4" v-model="current" :list="list" :mid-button="false"></u-tabbar>
+  <!-- v-model="current" -->
+  <u-tabbar
+    :height="100"
+    bg-color="#000"
+    inactive-color="#fff"
+    active-color="#2b85e4"
+    :list="list"
+    :mid-button="false"
+  ></u-tabbar>
 </template>
 <script setup>
-import {computed, ref} from "vue";
-import {useTabBarStore} from '@/store/modules/tabBar';
-import {useI18n} from "vue-i18n";
+import { computed, ref } from "vue";
+import { useTabBarStore } from "@/store/modules/tabBar";
+import { useI18n } from "vue-i18n";
 
-const {t} = useI18n();
+const pages = getCurrentPages();
+console.log("pages===", pages);
+
+const { t } = useI18n();
 const tabBarStore = useTabBarStore();
 const current = computed({
   get() {
-    return tabBarStore.getActiveTab
+    return tabBarStore.getActiveTab;
   },
   set(val) {
-    tabBarStore.SETCURRENT(val)
+    tabBarStore.SETCURRENT(val);
     if (val === 0) {
       uni.navigateTo({
-        url: '/pages/home/index'
-      })
+        url: "/pages/home/index",
+      });
     } else if (val === 1) {
       uni.navigateTo({
-        url: '/pages/hall/index'
-      })
+        url: "/pages/hall/index",
+      });
     } else if (val === 2) {
       uni.navigateTo({
-        url: '/pages/order/index'
-      })
+        url: "/pages/order/index",
+      });
     } else if (val === 3) {
       uni.navigateTo({
-        url: '/pages/my/index'
-      })
+        url: "/pages/my/index",
+      });
     }
-  }
-})
+  },
+});
 const list = ref([
   {
     iconPath: "home",
     selectedIconPath: "home-fill",
-    text: t('home'),
+    text: t("home"),
     isDot: true,
     customIcon: false,
+    pagePath: '/pages/home/index'
   },
   {
     iconPath: "bag",
     selectedIconPath: "bag-fill",
-    text: t('hall'),
+    text: t("hall"),
     customIcon: false,
+    pagePath: '/pages/hall/index'
   },
   {
     iconPath: "order",
     selectedIconPath: "order",
-    text: t('order'),
+    text: t("order"),
     customIcon: false,
+    pagePath: '/pages/order/index'
   },
   {
     iconPath: "account",
     selectedIconPath: "account-fill",
-    text: t('my'),
+    text: t("my"),
     isDot: false,
     customIcon: false,
+    pagePath: '/pages/my/index'
   },
-]); 
+]);
 </script>
