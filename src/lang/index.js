1e3e7796e79b625fb3318076bccf2ebf8df8f56d
topsuder <suders@vip.qq.com>
2024-07-25 19:50:19 +0800

    feat(承兑): i18n

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,8 +1,21 @@
-import { createI18n } from "vue-i18n";
+import {createI18n} from "vue-i18n";
 import enUs from "@/locale/en_US.json";
+import es from "@/locale/es.json";
+import fr from "@/locale/fr.json";
+import it from "@/locale/it.json";
+import jaJp from "@/locale/ja_JP.json";
+import koKr from "@/locale/ko_KR.json";
+import pt from "@/locale/pt.json";
 import deDE from "@/locale/de_DE.json";
+
 export const messages = {
   en_US: enUs, // 英语
+  es: es, // 英语
+  fr: fr, // 英语
+  it: it, // 英语
+  ja_JP: jaJp, // 英语
+  ko_KR: koKr, // 英语
+  pt: pt, // 英语
   de_DE: deDE, // 德语
 };
 uni.setStorageSync("language", uni.getStorageSync("language") || "en_US")
